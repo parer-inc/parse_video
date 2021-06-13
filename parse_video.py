634697e625ad83802c75a0836cb9a2e08963b9c8
max <maxfy@gmail.com>
2021-06-13 22:01:37 +0300

    parse video reworked: now uses requests

--- parse_video.py
+++ parse_video.py
@@ -1,21 +1,46 @@
 """This service allows to prase video info"""
 import os
 import time
+import json
+import re
 from rq import Worker, Queue, Connection
 from methods.connection import get_redis, await_job
 from pyyoutube import Api
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-driver = webdriver.Remote(
-command_executor='http://chromedriver:4444/wd/hub',
-desired_capabilities=DesiredCapabilities.CHROME)
+import requests
 
 YOUTUBE_URL = "https://www.youtube.com/watch?v="
 api = Api(api_key=os.environ['YOUTUBE_TOKEN'])
 r = get_redis()
+request = requests.sessions.Session()
+request.headers = {'X-YouTube-Client-Name': '1',
+                   'X-YouTube-Client-Version': '2.20201202.06.01',
+                   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.55'}
 
-def parse_video(id, coms = False):
+
+def get_basic_details(continuation, track_params, token):
+    req = request.post(
+        f'https://www.youtube.com/comment_service_ajax?action_get_comments=1&pbj=1&ctoken={continuation}&continuation={continuation}&itct={track_params}', data={'session_token': token}).text
+    a = json.loads(req)
+    count = str(a['response']['continuationContents']['itemSectionContinuation']
+                ['header']['commentsHeaderRenderer']['countText']['runs'][0]['text'])
+    count = ''.join([s for s in count if s.isdigit()])
+    page_len = len(a['response']['continuationContents']
+                   ['itemSectionContinuation']['contents'])
+    page_count = int(count) // page_len
+    return (req, int(count), page_len, page_count)
+
+
+def get_continuation_data(url):
+    html = request.get(url).text
+    a, b = re.search(
+        r'"continuation":"(.*?)","clickTrackingParams":"(.*?)"', html).groups()
+    b = b.replace('=', '%3D')
+    token = re.search(r'"XSRF_TOKEN":"(.*?)"',
+                      html).groups()[0].replace('\\u003d', '=')
+    return (a, b, token)
+
+
+def parse_video(id, coms=False):
     """Parses a video"""
     # GET VIEO DATA USING API
     channel_by_id = api.get_video_by_id(video_id=id)
@@ -30,34 +55,45 @@ def parse_video(id, coms = False):
             data['statistics']['dislikeCount'], data['statistics']['commentCount'],
             data['snippet']['description'], data['snippet']['channelId'],
             data['contentDetails']['duration'], data['snippet']['publishedAt'],
-            ','.join(data['snippet']['tags']), data['snippet']['defaultLanguage'],
+            ','.join(data['snippet']['tags']
+                     ), data['snippet']['defaultLanguage'],
             data['status']['madeForKids']]
     if coms:
-        driver.get(YOUTUBE_URL + id)
-        time.sleep(5)
-        height = driver.execute_script("return document.documentElement.scrollHeight")
-        print("Parsing comments")
-        try:
-            for i in range(1):#while True:
-                prev_ht = driver.execute_script("return document.documentElement.scrollHeight;")
-                driver.execute_script("window.scrollTo(0, " + str(height) + ");")
-                time.sleep(5)
-                height = driver.execute_script("return document.documentElement.scrollHeight")
-                print("done loop")
-                if prev_ht == height:
-                    break
-        except Exception as e:
-            print(e)  # LOG
-        try:
-            comms = driver.find_elements_by_xpath('//*[@id="video-title"]')
-            q = Queue('write_comment', connection=r)
-            for i in comms:
-                print(i)
-                # q.enqueue('write_comment.write_comment',)
-        except Exception as e:
-            print(e)  # LOG
-    else:
-        return data
+        url = YOUTUBE_URL + id
+        continuation, track_params, token = get_continuation_data(url)
+        initial_json, count, page_len, end_range = get_basic_details(
+            continuation, track_params, token)
+        continuation, track_params, token = '', '', ''
+
+        comments = []
+        for k in range(1):  # end_range
+            if k == 0:
+                json_data = initial_json
+            else:
+                json_data = request.post(
+                    f"https://www.youtube.com/comment_service_ajax?action_get_comments=1&pbj=1&ctoken={continuation}&continuation={continuation}&itct={track_params}", data={'session_token': token}).text
+            json_data = json.loads(json_data)
+            comment_json = json_data['response']['continuationContents']['itemSectionContinuation']['contents']
+            token = json_data['xsrf_token']
+            continuation = json_data['response']['continuationContents'][
+                'itemSectionContinuation']['continuations'][0]["nextContinuationData"]['continuation']
+            track_params = json_data['response']['continuationContents']['itemSectionContinuation'][
+                'continuations'][0]["nextContinuationData"]["clickTrackingParams"]
+            for i in comment_json:
+                com_data = i['commentThreadRenderer']['comment']['commentRenderer']
+                urnm = com_data['authorText']['simpleText']
+                txt = com_data['contentText']['runs'][-1]['text']
+                tm = com_data['publishedTimeText']['runs'][-1]['text']
+                lks = 0
+                rpls = 0
+                try:
+                    lks = com_data['voteCount']['simpleText']
+                    rpls = com_data['replyCount']
+                except Exception:
+                    pass
+                # WRITE INTO COMMENTS
+                print(urnm, txt, tm, lks, rpls)
+        return True
 
 
 if __name__ == '__main__':
